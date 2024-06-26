85cd1cbf133714a7510f5bb2f6663c84a5d35f14
CharC0de <eganocharlesreinerg@gmail.com>
2024-06-26 21:05:02 +0800

    First Commit

---
+++ app/build/generated/source/buildConfig/release/com/webhostapp/bitedash/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.webhostapp.bitedash.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.webhostapp.bitedash.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
